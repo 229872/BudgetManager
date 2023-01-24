ab0aad7f86156c24db97d80beb04df25b5adb4bb
Jakub Marszał <55239276+jcoby99@users.noreply.github.com>
2023-01-24 22:33:56 +0100

    add saving receipt photo in the storage

--- app/src/main/java/pl/lodz/budgetmanager/AddPurchaseActivity.java
+++ app/src/main/java/pl/lodz/budgetmanager/AddPurchaseActivity.java
@@ -1,18 +1,34 @@
 package pl.lodz.budgetmanager;
 
+import static android.content.ContentValues.TAG;
+import static pl.lodz.budgetmanager.repository.ReceiptRepository.receiptToMap;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,10 +36,8 @@ import java.util.List;
 import pl.lodz.budgetmanager.model.Category;
 import pl.lodz.budgetmanager.model.Purchase;
 import pl.lodz.budgetmanager.model.Receipt;
-import pl.lodz.budgetmanager.repository.ReceiptRepository;
 
 public class AddPurchaseActivity extends AppCompatActivity {
-    private final ReceiptRepository receiptRepository = ReceiptRepository.getInstance();
     private String shopName;
     private LocalDate purchaseDate;
     private Category category;
@@ -36,7 +50,64 @@ public class AddPurchaseActivity extends AppCompatActivity {
     private TextInputEditText quantityInput;
     private TextView purchaseStatus;
     private Button doneButton;
+    ActivityResultLauncher<PickVisualMediaRequest> pickMedia;
 
+    @SuppressLint("HardwareIds")
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Registers a photo picker activity launcher in single-select mode.
+        pickMedia = registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+                    // Callback is invoked after the user selects a media item or closes the
+                    // photo picker.
+                    if (uri != null) {
+                        Log.d("PhotoPicker", "Selected URI: " + uri);
+                        Intent intent = new Intent(this, MainActivity.class);
+                        Receipt receipt = new Receipt(shopName, purchaseList, purchaseDate, category);
+                        String deviceId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+
+                        FirebaseStorage storage = FirebaseStorage.getInstance();
+                        StorageReference storageRef = storage.getReference();
+                        byte[] fileBytes;
+                        try {
+                            fileBytes = getFileBytes(uri);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+                        db.collection("receipts")
+                                .add(receiptToMap(receipt, deviceId))
+                                .addOnSuccessListener(documentReference -> {
+                                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                    StorageReference riversRef = storageRef.child(documentReference.getId());
+                                    riversRef.putBytes(fileBytes);
+
+                                    startActivity(intent);
+
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(TAG, "Error adding document", e);
+                                    }
+                                });
+                    } else {
+                        Log.d("PhotoPicker", "No media selected");
+                    }
+                });
+    }
+
+    private byte[] getFileBytes(Uri uri) throws IOException {
+        InputStream inputStream = getContentResolver().openInputStream(uri);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = inputStream.read(buffer)) != -1) {
+            baos.write(buffer, 0, len);
+        }
+        return baos.toByteArray();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,9 +166,9 @@ public class AddPurchaseActivity extends AppCompatActivity {
 
     @SuppressLint("HardwareIds")
     public void createReceipt(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        Receipt receipt = new Receipt(shopName, purchaseList, purchaseDate, category);
-        receiptRepository.add(receipt, Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID));
-        startActivity(intent);
+        // Launch the photo picker and allow the user to choose only images.
+        pickMedia.launch(new PickVisualMediaRequest.Builder()
+                .setMediaType((ActivityResultContracts.PickVisualMedia.VisualMediaType) ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
+                .build());
     }
 }
